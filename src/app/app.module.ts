af726082109e9cc4cf09bee216ecf2b77e0c383f
Catalin <uzy_electro@yahoo.com>
2022-08-24 11:16:21 +0300

    task form body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { DialogModule } from "primeng/dialog";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { ButtonModule } from "primeng/button";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { HomePageComponent } from "./home-page/home-page.component";
+import { TaskFormComponent } from "./task-form/task-form.component";
+
+
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HomePageComponent
+		HomePageComponent,
+		TaskFormComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		FormsModule,
+		ButtonModule,
+		DialogModule,
+		BrowserAnimationsModule,
+		ReactiveFormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
 	selector: "home-page",
 	templateUrl: "./home-page.component.html",
 	styleUrls: ["./home-page.component.css"]
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	public constructor() { }
-
-	public ngOnInit(): void {
-		console.log("home page work!");
-	}
 
 }
