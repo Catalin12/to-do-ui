1f41b33621215f77184d33caf35b0e3da01118a0
carmina-c <carmina.cotiga23@gmail.com>
2022-08-30 17:16:33 +0300

    Import order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MenubarModule } from "primeng/menubar";
 import { DialogModule } from "primeng/dialog";
 import { ButtonModule } from "primeng/button";
 import { ToastModule } from "primeng/toast";
+import { MessageService } from "primeng/api";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -23,7 +24,6 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { MessageService } from "primeng/api";
 
 @NgModule({
 	declarations: [
