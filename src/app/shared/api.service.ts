9727ae056c5cb02fb620b9f69d452d629be35a60
carmina-c <carmina.cotiga23@gmail.com>
2022-08-31 12:01:35 +0300

    Token added to headers

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 import { TaskDTO } from "./task.dto";
+import { LocalStorageService } from "./local-storage.service";
 
 @Injectable({
 	providedIn: "root"
@@ -11,30 +12,48 @@ export class ApiService {
 
 	private apiURL: string = "http://localhost:3000/task";
 
-	public constructor(private http: HttpClient) { }
+	public constructor(
+		private http: HttpClient,
+		private localStorageService: LocalStorageService
+	) { }
 
 	public getAllTasks(): Observable<TaskDTO[]> {
 		const getAllTasksUrl = this.apiURL + "/all";
-		return this.http.get<TaskDTO[]>(getAllTasksUrl);
+		const token = this.localStorageService.getToken();
+		return this.http.get<TaskDTO[]>(getAllTasksUrl, {
+			headers: { "Authorization": "Bearer " + token, "Content-Type": "application/json" }
+		});
 	}
 
 	public addTask(task: TaskDTO): Observable<TaskDTO> {
 		const addTaskUrl = this.apiURL + "/new-task";
-		return this.http.post(addTaskUrl, task);
+		const token = this.localStorageService.getToken();
+		return this.http.post(addTaskUrl, task, {
+			headers: { "Authorization": "Bearer " + token, "Content-Type": "application/json" }
+		});
 	}
 
 	public editTask(task: TaskDTO): Observable<TaskDTO> {
 		const editTaskUrl = this.apiURL + "/edit-task/" + task.id;
-		return this.http.put(editTaskUrl, task);
+		const token = this.localStorageService.getToken();
+		return this.http.put(editTaskUrl, task, {
+			headers: { "Authorization": "Bearer " + token, "Content-Type": "application/json" }
+		});
 	}
 
 	public changeTaskStatus(id: number): Observable<TaskDTO> {
 		const changeTaskStatusUrl = this.apiURL + "/new-status/" + id;
-		return this.http.patch(changeTaskStatusUrl, {id});
+		const token = this.localStorageService.getToken();
+		return this.http.patch(changeTaskStatusUrl, {id}, {
+			headers: { "Authorization": "Bearer " + token, "Content-Type": "application/json" }
+		});
 	}
 
 	public deleteTask(id: number): Observable<TaskDTO> {
 		const deleteTaskUrl = this.apiURL + "/delete/" + id;
-		return this.http.patch(deleteTaskUrl, {id});
+		const token = this.localStorageService.getToken();
+		return this.http.patch(deleteTaskUrl, {id}, {
+			headers: { "Authorization": "Bearer " + token, "Content-Type": "application/json" }
+		});
 	}
 }
